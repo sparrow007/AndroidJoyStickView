b6d666f846879d5ab9125a79ffe4f5d189d94b91
Hannes Achleitner <hannes.software@gmx.at>
2022-11-20 08:09:55 +0100

    Code cleanup

--- joystickview/src/main/java/com/jackandphantom/joystickview/OuterCircleView.java
+++ joystickview/src/main/java/com/jackandphantom/joystickview/OuterCircleView.java
@@ -4,17 +4,18 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+
 import androidx.annotation.Nullable;
+
 import android.util.AttributeSet;
 import android.view.View;
 
 class OuterCircleView extends View {
 
-
     private float centerPoint;
     private float circleRadius;
-    private Paint circlePaint = new Paint();
-    private Paint borderCirclePaint = new Paint();
+    private final Paint circlePaint = new Paint();
+    private final Paint borderCirclePaint = new Paint();
     private boolean changeLayoutManual;
     private int width, height;
     private float shadowRadius = 9.0f;
@@ -22,17 +23,13 @@ class OuterCircleView extends View {
     //This makes circle smaller as much we want
     private float circleFactor = 0.4f;
     private int shadowColor = Color.RED;
-    private int min;
-
 
     public OuterCircleView(Context context) {
         super(context);
-
     }
 
     public OuterCircleView(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
-
     }
 
     public OuterCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -41,11 +38,9 @@ class OuterCircleView extends View {
     }
 
     /*
-    * Initialize paint attributes to add effect on circle
-    * */
-
+     * Initialize paint attributes to add effect on circle
+     * */
     private void init() {
-
         circlePaint.setAntiAlias(true);
         circlePaint.setColor(Color.WHITE);
         circlePaint.setStyle(Paint.Style.FILL);
@@ -53,68 +48,62 @@ class OuterCircleView extends View {
         borderCirclePaint.setAntiAlias(true);
         borderCirclePaint.setStyle(Paint.Style.STROKE);
         setShadow();
-
     }
 
     /*
-    * @variable changeLayoutManual means we are changing the width and height of OuterCircleView
-    * so when we want to change it's layout size then we only need to make changeLayoutManual to true
-    * also calculating the radius and centerPoint for drawing circle
-    * */
+     * @variable changeLayoutManual means we are changing the width and height of OuterCircleView
+     * so when we want to change it's layout size then we only need to make changeLayoutManual to true
+     * also calculating the radius and centerPoint for drawing circle
+     * */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-        if(changeLayoutManual) {
+        if (changeLayoutManual) {
             setMeasuredDimension(width, height);
         } else {
-             width = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
-             height = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+            width = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+            height = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
             setMeasuredDimension(width, height);
         }
 
-         min = Math.min(width, height);
-        centerPoint = min/2;
-        circleRadius = min*circleFactor;
-
-    }
-
-   /*
-   * first circle is works as background for joystick and second one is for border of the first circle
-   * */
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        canvas.drawCircle(centerPoint, centerPoint, circleRadius, borderCirclePaint);
-        canvas.drawCircle(centerPoint , centerPoint, circleRadius , circlePaint);
-
+        int min = Math.min(width, height);
+        centerPoint = min / 2;
+        circleRadius = min * circleFactor;
     }
 
     /*
-    * This method will call when we need to change size of this view
-    * */
-    public void setWidthAndHeight(int width , int height) {
+     * first circle is works as background for joystick and second one is for border of the first circle
+     * */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(centerPoint, centerPoint, circleRadius, borderCirclePaint);
+        canvas.drawCircle(centerPoint, centerPoint, circleRadius, circlePaint);
+    }
 
+    /*
+     * This method will call when we need to change size of this view
+     * */
+    public void setWidthAndHeight(int width, int height) {
         changeLayoutManual = true;
-        this.width = width*2;
-        this.height = height*2;
+        this.width = width * 2;
+        this.height = height * 2;
         requestLayout();
     }
 
-    private  void setShadow() {
-
+    private void setShadow() {
         setLayerType(LAYER_TYPE_SOFTWARE, borderCirclePaint);
         borderCirclePaint.setShadowLayer(shadowRadius, -dx, dy, shadowColor);
     }
 
-    /**   Setter and Getter methods         ****/
-
-    void setBorderStrokeWidth (float width) {
+    /**
+     * Setter and Getter methods
+     ****/
+    void setBorderStrokeWidth(float width) {
         borderCirclePaint.setStrokeWidth(width);
         invalidate();
     }
 
-    void setShadowRadius (float radius) {
+    void setShadowRadius(float radius) {
         this.shadowRadius = radius;
         setShadow();
         invalidate();
@@ -125,7 +114,7 @@ class OuterCircleView extends View {
         invalidate();
     }
 
-    void setBorderCircleColor (int color) {
+    void setBorderCircleColor(int color) {
         borderCirclePaint.setColor(color);
         invalidate();
     }
@@ -137,7 +126,7 @@ class OuterCircleView extends View {
         invalidate();
     }
 
-    void setShadowColor (int color) {
+    void setShadowColor(int color) {
         this.shadowColor = color;
         setShadow();
         invalidate();
